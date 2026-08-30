8a5d9c0f6ae804641fb2ed4d285ce3be52d83703
lenz-godie-11 <emmanueldaud358@gmail.com>
2026-08-31 00:53:56 +0300

    commit

--- MainGame/main.py
+++ MainGame/main.py
@@ -39,3 +39,33 @@ def score_on_screen(text,color,x,y):
         snake_y=55
         velocity_x= 0 
         velocity_y= 0 
+        init_velocity = 5
+        score= 0 
+        apple_x = random.randint(20,width_of_screen/2) 
+        apple_y = random.randint(20,height_of_screen/2)
+        snake_size=30 
+        snake_list = [] 
+        snake_length = 1 
+        fps=40
+
+        with open ("highscore.txt","r") as f: 
+            highscore = f.read()
+            while not game_exit:
+                if game_over:
+                    with open ("high score.txt","w") as f:
+                        f.write(str(highscore))
+                        gameWindow.fill(white) 
+                        score_on_screen("Game Over! Press Enter to continue",red,100,250) 
+                        for event in pygame.event.get(): 
+                            if event.type == pygame.QUIT: 
+                                game_exit = True if event.type== pygame.KEYDOWN: 
+                                if event.key == pygame.K_RETURN:
+                                    welcome()
+                                    else: 
+                                    for event in pygame.event.get(): 
+                                        if event.type == pygame.QUIT: 
+                                            game_exit = True if event.type==pygame.KEYDOWN:
+                                    if event.key == pygame.K_RIGHT: 
+                                        velocity_x = init_velocity 
+                                        velocity_y = 0 if event.key == pygame.K_LEFT: 
+                                        velocity_x = -init_velocity
